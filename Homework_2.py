9b181419980e052be914c2ea931736509d123563
Dju-Jetlearn <pavelalcaraz@gmail.com>
2025-01-18 12:08:46 +0000

    Add files via upload

---
+++ Homework_2.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+noone = np.random.randint(1,101)
+print(noone)
+
+notwo = np.random.permutation(np.arange(1,101))
+print(notwo)
+
+nothree = np.random.rand(1,101)
+print(nothree)
+
+nofour = np.random.randint(24,543)
+print(nofour)
+
+nofive = np.random.permutation(np.arange(234,654))
+print(nofive)
+
+nosix = np.random.rand(1,345)
+print(nosix)
+
+noseven = np.random.randint(56,787)
+print(noseven)
+
+noeight = np.random.permutation(np.arange(34,876))
+print(noeight)
+
+nonine = np.random.rand(1,876)
+print(nonine)
